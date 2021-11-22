53f90586ccd07e1ad318d36be34b95d2d4745a60
Kayn <morak0398@gmail.com>
2021-11-22 15:00:23 +0200

    finished 030_js_functions 010/010

--- 030_js_functions/scripts/script.js
+++ 030_js_functions/scripts/script.js
@@ -174,9 +174,10 @@ document.querySelector('.ninth-task__button').onclick = function () {
     // получаем ввод секунд от пользователя
     let aa = parseInt(prompt('Введите секунды'));
 
-    // объявляю функцию
+
     let calcHrsMinSec = function (a) {
-        // переменные для секунд, минут и часов (отдельно)
+
+    // переменные для секунд, минут и часов (отдельно)
     let seconds = a;
     let minutes;
     let hours;
@@ -232,4 +233,74 @@ document.querySelector('.ninth-task__button').onclick = function () {
     document.querySelector('.ninth-task__result-output').innerHTML = 'Конвертированное время = ' + resultOutput;
 }
 
-// task 10 - написать функцию которая высчитывает разницу между датами
+// task 10 - написать функцию которая высчитывает разницу между time'ами
+/* 
+Написать функцию, которая считает разницу между двумя значениями времени. 
+Функция принимает 6 параметров которые описывают 2 времени, 
+и возвращает результат в виде строки «чч:мм:сс». 
+При выполнении задания используйте функции из предыдущих 2-х заданий: 
+сначала обе даты переведите в секунды, узнайте разницу в секундах, а потом разницу 
+переведите обратно в «чч:мм:сс»
+*/
+
+document.querySelector('.tenth-task__button').onclick = function () {
+    let aa = parseInt(prompt('Введите первое значение в секундах'));
+    let ab = parseInt(prompt('Введите второе значение в секундах'));
+
+    let calcHrsMinSec = function (a,b) {
+        console.log(`От ${a} сек отнимаем ${b} сек, результат ↓`);
+        a = a - b;
+        // переменные для секунд, минут и часов (отдельно)
+        let seconds = a;
+        let minutes;
+        let hours;
+    
+        // делю секунды на 60 - т.е. перевожу в минуты и округляю значение вниз
+        minutes = Math.floor(seconds / 60);
+        // остаток от перевода в минуты присваиваю секундам
+        seconds = seconds % 60;
+    
+        hours = Math.floor(minutes / 60);
+        minutes = (minutes % 60);
+    
+        let massiv = [];
+        massiv.push(hours);     massiv.push(minutes);     massiv.push(seconds);
+        return massiv;
+    }
+
+    let displayHrsMinSec = function (a) {
+        // переменная для промежуточных вычислений
+        let result = '';
+        let dvoetochie = ':';
+        for (let i = 0; i < a.length; i++) {
+            // проверка не является ли значение массива последним
+            if (i !== (a.length - 1)) {
+                // проверка состоит число из двух цифр или нет
+                if (a[i] < 10) {
+                    // если состоит из одной цифры то перед значением вставляем 0
+                    result = result + '0' + a[i] + dvoetochie;
+                } else {
+                    // если из двух цифр, то 0 не вставляем 
+                    // (число и так подходит под формат чч:мм:сс)
+                    result = result + a[i] + dvoetochie;
+                }
+            } else {
+                // всё то же что и в if ↑↑↑
+                if (a[i] < 10) {
+                    // если состоит из одной цифры то перед значением вставляем 0
+                    result = result + '0' + a[i];
+                } else {
+                    // если из двух цифр то 0 не вставляем 
+                    // (число и так подходит под формат чч:мм:сс)
+                    result = result + a[i];
+                }
+            }
+        } 
+        // выходим из цикла for
+        return result;
+    }
+
+    let resultOutput = displayHrsMinSec(calcHrsMinSec(aa,ab));
+    console.log('Конвертированное время = ' + resultOutput);
+    document.querySelector('.tenth-task__output-one').innerHTML = `Результат время = ${resultOutput}`;
+}
