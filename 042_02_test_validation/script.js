283c27cf48861f95d007af61a38c2810e7dccec4
Kayn <morak0398@gmail.com>
2021-12-21 23:30:47 +0200

    made inputs validation 042

---
+++ 042_02_test_validation/script.js
@@ -0,0 +1,19 @@
+const regEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+const inputEmail = document.querySelector('#typeEmail');
+const button = document.querySelector('#button1');
+
+
+button.onclick = function (e) {
+    e.preventDefault();
+    if (!validate(regEmail, inputEmail.value)) {
+        alert('no');
+    }
+    else {
+        console.log('yes');
+    }
+}
+
+function validate (regex, inp) {
+    return regex.test(inp);
+}
--- 042_exam_2/js/script.js
+++ 042_exam_2/js/script.js
@@ -40,3 +40,39 @@ contactLink.addEventListener('click', function(event) {
     let projectCoords = document.querySelector('.contacts').getBoundingClientRect().top;
     scrollToSmth(projectCoords);
 });
+
+const regEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const regName = /^[a-z]([-']?[a-z]+)*( [a-z]([-']?[a-z]+)*)+$/;
+const inpName = document.getElementById('input-name');
+const inpEmail = document.getElementById('input-email');
+
+document.querySelector('.contacts__submit').onclick = function (e) {
+    e.preventDefault();
+    if (!validate(regName, inpName.value)) {
+        invalid(inpName);
+    }
+    else {
+        valid(inpName);
+    }
+    if (!validate(regEmail, inpEmail.value)) {
+        invalid(inpEmail);
+    }
+    else {
+        valid(inpEmail);
+    }
+
+    function validate (regex, input) {
+        return regex.test(input);
+    }
+
+    function valid (input) {
+        input.classList.remove('invalid-input');
+        input.classList.add('valid-input');
+    }
+
+    function invalid (input) {
+        alert(`Input ${input.value} is invalid.`);
+        input.classList.add('invalid-input');
+        input.classList.remove('valid-input');
+    }
+}
