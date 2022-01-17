095bd3c961e2f2f1656c23a432691961eb8636a0
Kayn <morak0398@gmail.com>
2022-01-17 18:19:08 +0200

    working on 050_exam_3

--- 050_exam_3/app/js/main.js
+++ 050_exam_3/app/js/main.js
@@ -47,4 +47,68 @@ let sliderWork = function () {
 	
 }
 
+let launchPosts = function () {
+	let currentActivePost = document.querySelector('.post.active');
+	const postsContainer = document.querySelector('.posts');
+	let activePage;
+
+
+	let selectPageButtons = document.querySelectorAll('.pages__link');
+	for (let i = 0; i < selectPageButtons.length; i++) {
+		selectPageButtons[i].onclick = function (e) {
+			e.preventDefault();
+
+			for (let j = 0; j < selectPageButtons.length; j++) {
+				if (selectPageButtons[j].classList.contains('active')) {
+					activePage = selectPageButtons[j];
+				}
+			}
+
+
+			function switchPage () {
+				let postsForSwitch = document.querySelectorAll('.post');
+				
+				currentActivePost = document.querySelector('.post.active');
+
+
+				activePage.classList.toggle('active');
+				currentActivePost.classList.toggle('active')
+				selectPageButtons[i].classList.toggle('active');
+				postsForSwitch[i].classList.toggle('active');
+			}
+
+
+			// activePage.classList.toggle('active');
+			// selectPageButtons[i].classList.toggle('active');
+			switchPage();
+			killClones();
+			clonePost();
+		}
+	}
+
+
+	function clonePost () {
+		// клонирование узла currentActivePost со всеми детьми и 
+		// вставка в postsContainer
+		currentActivePost = document.querySelector('.post.active')
+		for (let i = 1; i <= 3; i++) {
+			let clonedPost = currentActivePost.cloneNode(true);
+			clonedPost.classList.add('clone');
+			postsContainer.appendChild(clonedPost);
+		}
+
+
+	}
+
+	function killClones () {
+		let clonesList = document.querySelectorAll('.clone');
+		for (let i = 0; i < clonesList.length; i++) {
+			clonesList[i].remove();
+		}
+	}
+
+	clonePost();
+}
+
 sliderWork();
+launchPosts();
